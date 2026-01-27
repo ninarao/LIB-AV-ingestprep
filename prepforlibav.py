b75febb767ad94c68ab7d7c881ef93ccbbc40450
Nina <nina.rao@gmail.com>
2026-01-27 09:38:38 -0500

    Add files via upload

--- prepforlibav.py
+++ prepforlibav.py
@@ -19,8 +19,8 @@ def setup(args_):
     parser = argparse.ArgumentParser(
         description='test description')
     parser.add_argument(
-        'destination',
-        help='Staging directory'
+        'source',
+        help='directory of files'
     )
     args = parser.parse_args(args_)
     return args
@@ -40,99 +40,95 @@ def ask_yes_no(question):
         elif answer in ('N,' 'n'):
             return 'N'
 
-def copy_to_stage(destination):
-    source = input('\n\n**** enter directory to copy from (with full path):     ')
-    if not os.path.isdir(source):
-        print(f"no directory {source} exists, returning to main menu")
+def copy_to_stage(source):
+    dest = input('\n\n**** enter directory to copy to (with full path):     ')
+    if not os.path.isdir(dest):
+        print(f"no directory {dest} exists, making directory")
+        os.makedirs(dest)
+    source = os.path.abspath(source)
+    sourceContents = source + '/'
+    cmd_dryrun = [
+        'rsync', '--dry-run',
+        '-ah', '-vv',
+        '--exclude=.*', '--exclude=.*/', '--exclude=*DS_Store', 
+        '--progress', '--stats',
+        sourceContents, dest
+    ]
+    print(f"dry run of copy to stage command: {cmd_dryrun}")
+    subprocess.call(cmd_dryrun)
+    run_copy = ask_yes_no('continue with copy to staging location?')
+    if run_copy == 'Y':
+        cmd_copy = [
+            'rsync', 
+            '-ah', '-vv',
+            '--exclude=.*', '--exclude=.*/', '--exclude=*DS_Store', 
+            '--progress', '--stats',
+            sourceContents, dest
+        ]
+        print(cmd_copy)
+        subprocess.call(cmd_copy)
+        print('verifying checksums match...')
+        sourceContents = source + '/'
+        cmd_checkmatch = [
+            'rsync',
+            '--dry-run',
+            '--checksum',
+            '-ah', '-vv',
+            '--progress',
+            sourceContents, dest
+        ]
+        print(cmd_checkmatch)
+        subprocess.call(cmd_checkmatch)
         return
     else:
-        if not os.path.isdir(destination):
-            print(f"no directory {destination} exists, making directory")
-            os.makedirs(destination)
-        source = os.path.abspath(source)
-        sourceContents = source + '/'
-        cmd_dryrun = [
-            'rsync', '--dry-run',
-            '-ah', '-vv',
-            '--exclude=.*', '--exclude=.*/',
-            '--progress', '--stats',
-            sourceContents, destination
-        ]
-        print(f"dry run of copy to stage command: {cmd_dryrun}")
-        subprocess.call(cmd_dryrun)
-        run_copy = ask_yes_no('continue with copy to staging drive?')
-        if run_copy == 'Y':
-            cmd_copy = [
-                'rsync', 
-                '-ah', '-vv',
-                '--exclude=.*', '--exclude=.*/',
-                '--progress', '--stats',
-                sourceContents, destination
-            ]
-            print(cmd_copy)
-            subprocess.call(cmd_copy)
-            print('verifying checksums match...')
-            sourceContents = source + '/'
-            cmd_checkmatch = [
-                'rsync',
-                '--dry-run',
-                '--checksum',
-                '-ah', '-vv',
-                '--progress',
-                sourceContents, destination
-            ]
-            print(cmd_checkmatch)
-            subprocess.call(cmd_checkmatch)
-            return
-        else:
-            print('skipping copy and returning to main menu')
-            return
+        print('skipping copy and returning to main menu')
+        return
 
-def get_video_files(destination):
+def get_video_files(source):
     video_list = []
-    if os.path.isdir(destination):
-        folder_list = os.listdir(destination)
+    if os.path.isdir(source):
+        folder_list = os.listdir(source)
         for filename in folder_list:
             if not filename[0] == '.':
                 if filename.lower().endswith(('.MOV', '.mov', 'MP4', '.mp4', '.mkv', '.MXF', '.mxf', '.dv', '.DV', '.MTS', '.swf', '.avi', '.m2t')):
-                    video_list.append(os.path.join(destination, filename))
-    elif os.path.isfile(destination):
-        video_list = [destination]
+                    video_list.append(os.path.join(source, filename))
+    elif os.path.isfile(source):
+        video_list = [source]
     return video_list
 
-def get_audio_files(destination):
+def get_audio_files(source):
     audio_list = []
-    if os.path.isdir(destination):
-        folder_list = os.listdir(destination)
+    if os.path.isdir(source):
+        folder_list = os.listdir(source)
         for filename in folder_list:
             if not filename[0] == '.':
                 if filename.lower().endswith(('.WAV', '.wav', '.aiff', '.AIFF', '.mp3', '.MP3')):
-                    audio_list.append(os.path.join(destination, filename))
-    elif os.path.isfile(destination):
-        audio_list = [destination]
+                    audio_list.append(os.path.join(source, filename))
+    elif os.path.isfile(source):
+        audio_list = [source]
     return audio_list
         
-def get_checksum_files(destination):
+def get_checksum_files(source):
     checksum_list = []
-    if os.path.isdir(destination):
-        folder_list = os.listdir(destination)
+    if os.path.isdir(source):
+        folder_list = os.listdir(source)
         for filename in folder_list:
             if not filename[0] == '.':
                 if filename.lower().endswith(('.MD5', '.md5', '.SHA1', '.sha1', '.TXT', '.txt')):
-                    checksum_list.append(os.path.join(destination, filename))
-    elif os.path.isfile(destination):
-        checksum_list = [destination]
+                    checksum_list.append(os.path.join(source, filename))
+    elif os.path.isfile(source):
+        checksum_list = [source]
     return checksum_list
 
-def get_all_files(destination):
-    video_list = get_video_files(destination)
-    audio_list = get_audio_files(destination)
-    checksum_list = get_checksum_files(destination)
+def get_all_files(source):
+    video_list = get_video_files(source)
+    audio_list = get_audio_files(source)
+    checksum_list = get_checksum_files(source)
     all_files = video_list + audio_list + checksum_list
     return all_files
 
-def rename_menu(destination):
-    print(f'staging directory: {destination}')
+def rename_menu(source):
+    print(f'staging directory: {source}')
     print('\nWhat would you like to do?')
     print('1. Prepend string to filenames')
     print('2. Replace string in filenames')
@@ -140,32 +136,32 @@ def rename_menu(destination):
     print('Q. Quit to main menu')
     print('\n')
     
-def rename_files(destination):
+def rename_files(source):
     while True:
-        rename_menu(destination)
+        rename_menu(source)
         choice = input('Enter your option: ').strip().upper()
         if choice == '1':
-            prepend(destination)
+            prepend(source)
         elif choice == '2':
-            middle(destination)
+            middle(source)
         elif choice == '3':
-            add_suffix(destination)
+            add_suffix(source)
         elif choice in ['Q', 'q']:
             print(' - Returning to main menu')
             break
         else:
             print(' - Incorrect input. Please enter 1, 2, 3, or Q')
 
-def prepend(destination):
+def prepend(source):
     while True:
-        all_files = get_all_files(destination)
+        all_files = get_all_files(source)
         header = input('\n**** enter string to be prepended to filenames or type Q to quit to menu\n\n')
         if header in ['Q', 'q']:
             print('returning to menu')
             break
         else:
             print(f'you entered: "{header}"')
-            os.chdir(destination)
+            os.chdir(source)
             print('building file list...')
             for file in all_files:
                 justName = Path(file).stem
@@ -184,9 +180,9 @@ def prepend(destination):
             else:
                 print('\ntry again')
 
-def middle(destination):
+def middle(source):
     while True:
-        all_files = get_all_files(destination)
+        all_files = get_all_files(source)
         middle_orig = input('\n**** enter string to be replaced in filenames or type Q to quit to menu\n\n')
         if middle_orig in ['Q', 'q']:
             print('returning to menu')
@@ -194,7 +190,7 @@ def middle(destination):
         else:
             middle_new = input('\n**** enter replacement string\n')
             print(f'replace "{middle_orig}" with "{middle_new}"')
-            os.chdir(destination)
+            os.chdir(source)
             print('building file list...')
             for file in all_files:
                 justName = Path(file).stem
@@ -215,7 +211,7 @@ def middle(destination):
             else:
                 print('\ntry again')
         
-def add_suffix(destination):
+def add_suffix(source):
     while True:
         suffix = ''
         file_ext = input('\n**** enter extension of files or type Q to quit to menu\n\n')
@@ -238,7 +234,7 @@ def add_suffix(destination):
                     suffix = ''
                     print('\n**** invalid input.')
             print(f'add "_{suffix}" to files ending in ".{file_ext}"')
-            os.chdir(destination)
+            os.chdir(source)
             print('building file list...')
             for file in glob.glob(f"*.{file_ext}"):
                 justName = Path(file).stem
@@ -258,21 +254,21 @@ def add_suffix(destination):
                 print('\ntry again')
                 return file_ext, suffix
 
-def rename_stage(destination):
-    os.chdir(destination)
+def rename_stage(source):
+    os.chdir(source)
     print('\n\nstaging directory should be named in this format: MSSXXXX_YYYY_MM_DD \
             \n\nXXXX = collection number \
             \n\n**for EUA, Oxford, and Pitts use SER or RG instead of MSS as needed**')
-    old_path, old_name = os.path.split(destination)
+    old_path, old_name = os.path.split(source)
     new_name = input('\n\n**** enter new name for directory:     ').upper()
     rename = ask_yes_no(f'rename directory from {old_name} to {new_name}?')
     if rename == 'N':
         print('skipping rename')
     elif rename == 'Y':
         new_dest = os.path.join(old_path, new_name)
-        print(f'renaming {destination} to {new_dest}')
+        print(f'renaming {source} to {new_dest}')
         try:
-            os.rename(destination, new_dest)
+            os.rename(source, new_dest)
             return new_dest
         except NotADirectoryError:
             print('not a directory')
@@ -288,7 +284,7 @@ def move_to_top(maindir):
             subdir_path = os.path.join(maindir,dir)
             subs.append(subdir_path)
     if not subs:
-        print('no subdirectories, returning to main menu')
+        print('no subdirectories, returning to main menu\n')
         return
     else:
         print('subdirectories found:')
@@ -323,18 +319,18 @@ def move_to_top(maindir):
             print('skipping file move.')
             return
 
-def get_mediainfo(destination):
-    video_list = get_video_files(destination)
-    audio_list = get_audio_files(destination)
-    checksum_list = get_checksum_files(destination)
+def get_mediainfo(source):
+    video_list = get_video_files(source)
+    audio_list = get_audio_files(source)
+    checksum_list = get_checksum_files(source)
     
     csv_location = input('\n\n**** enter csv location or leave blank to create csv:     ')
     if csv_location != '':
         csv_file = os.path.abspath(csv_location)
     else:
-        csv_dir = os.path.basename(destination)
+        csv_dir = os.path.basename(source)
         csvv = 'Object_CSV_' + csv_dir + '.csv'
-        csv_file = os.path.join(destination, csvv)
+        csv_file = os.path.join(source, csvv)
         
     csv_basename = os.path.basename(csv_file)
     csv_path = os.path.dirname(csv_file)
@@ -420,7 +416,7 @@ def arrange_csv(csv_file, header_list):
     df.insert(12, 'intermediate_file_note', '')
     df.insert(13, 'service_file_note', '')
     df.insert(14, 'emory_ark', '')
-    df.insert(15, 'emory_ark2', '')
+    df.insert(15, 'abstract', '')
     df.to_csv(csv_file, index=False, header=True)
     df = pd.read_csv(csv_file)
     
@@ -458,13 +454,10 @@ def arrange_csv(csv_file, header_list):
     mergedd_df['intermediate_file_x'] = mergedd_df['intermediate_file_y']
     mergedd_df['intermediate_file_note_x'] = mergedd_df['intermediate_file_note_y']
     mergedd_df = mergedd_df.drop(columns=['fileset_label', 'extent', 'technical_note', 'service_file_y', 'intermediate_file_y', 'service_file_note_y', 'intermediate_file_note_y'])
-    mergedd_df = mergedd_df.rename(columns={'fileset_label_x': 'fileset_label'})
-    mergedd_df = mergedd_df.rename(columns={'extent_x': 'extent'})
-    mergedd_df = mergedd_df.rename(columns={'technical_note_x': 'technical_note'})
-    mergedd_df = mergedd_df.rename(columns={'service_file_x': 'service_file'})
-    mergedd_df = mergedd_df.rename(columns={'intermediate_file_x': 'intermediate_file'})
-    mergedd_df = mergedd_df.rename(columns={'service_file_note_x': 'service_file_note'})
-    mergedd_df = mergedd_df.rename(columns={'intermediate_file_note_x': 'intermediate_file_note'})
+    mergedd_df = mergedd_df.rename(columns={'fileset_label_x': 'fileset_label', 'extent_x': 'extent',
+                                            'technical_note_x': 'technical_note', 'service_file_x': 'service_file',
+                                            'intermediate_file_x': 'intermediate_file', 'service_file_note_x': 'service_file_note',
+                                            'intermediate_file_note_x': 'intermediate_file_note'})
     mergedd_df['staff_notes'] = pd.NA
     mergedd_df['type'] = mergedd_df['type'].mask(pd.isnull(mergedd_df['type']), 'fileset') 
     mergedd_df['pcdm_use'] = mergedd_df['pcdm_use'].mask(pd.isnull(mergedd_df['pcdm_use']), 'Primary Content') 
@@ -500,8 +493,7 @@ def arrange_csv(csv_file, header_list):
     mergeddv_df['preservation_master_file'] = mergeddv_df['master_file_note']
     mergeddv_df['service_file'] = mergeddv_df['service_file_note_y']
     mergeddv_df['intermediate_file'] = mergeddv_df['intermediate_file_note_y']
-    mergeddv_df = mergeddv_df.rename(columns={'service_file_note_x': 'service_file_note'})
-    mergeddv_df = mergeddv_df.rename(columns={'intermediate_file_note_x': 'intermediate_file_note'})
+    mergeddv_df = mergeddv_df.rename(columns={'service_file_note_x': 'service_file_note', 'intermediate_file_note_x': 'intermediate_file_note'})
     mergeddv_df['master_file_note'] = pd.NA
     mergeddv_df['intermediate_file_note'] = pd.NA
     mergeddv_df['service_file_note'] = pd.NA
@@ -514,12 +506,37 @@ def arrange_csv(csv_file, header_list):
     mergeddv_df.to_csv(csv_file, index=False, header=True)
 
     df_combined = pd.concat([mergedd_df, mergeddv_df], ignore_index=True)
-    df_combined.to_csv(csv_file, index=False, header=True)
+    df_combined.insert(0, 'deduplication_key', '')
+    df_combined.insert(1, 'other_identifiers', '')
+    df_combined.insert(3, 'title', '')
+    df_combined.insert(4, 'holding_repository', '')
+    df_combined.insert(5, 'content_type', '')
+    df_combined.insert(11, 'extracted', '')
+    df_combined.insert(12, 'transcript_file', '')
+    df_combined.insert(13, 'content_genres', '')
+    df_combined.insert(15, 'sublocation', '')
+    df_combined.insert(17, 'local_call_number', '')
+    df_final = df_combined.assign(administrative_unit='', contact_information='', creator='',
+                                  date_created='', date_issued='', content_genres='', institution='',
+                                  primary_language='', notes='', place_of_production='', publisher='',
+                                  emory_rights_statements='', rights_statement='', subject_names='',
+                                  subject_geo='', keywords='', subject_topics='', uniform_title='',
+                                  date_classifications='', visibility='', copyright_date='',
+                                  internal_rights_note='', rights_holders='', legacy_rights='',
+                                  sensitive_material='', sensitive_material_note='', Ingestworkflow_notes='',
+                                  Ingestworkflow_rights_basis='', Ingestworkflow_rights_basis_date='',
+                                  Ingestworkflow_rights_basis_note='', RightsAccessLevel='',
+                                  Accessionworkflow_rights_basis='', Accessionworkflow_rights_basis_date='',
+                                  Accessionworkflow_rights_basis_reviewer='', Accessionworkflow_rights_basis_note='')
+    df_final.columns = df_final.columns.str.replace('Ingestw', 'Ingest.w')
+    df_final.columns = df_final.columns.str.replace('Accessionw', 'Accession.w')
+    df_final.columns = df_final.columns.str.replace('RightsAccessLevel', 'Rights - Access Level')
+    df_final.to_csv(csv_file, index=False, header=True)
 
-def main_menu(destination):
-    print(f'staging directory: {destination}')
+def main_menu(source):
+    print(f'current staging directory: {source}')
     print('\nWhat would you like to do?')
-    print('1. Copy files to staging directory')
+    print('1. Copy files to another location')
     print('2. Rename files')
     print('3. Rename staging directory')
     print('4. Move files to top-level directory')
@@ -531,25 +548,28 @@ def main(args_):
     stage_dir = ''
     while True:
         if stage_dir != '':
-            destination = new_dest
+            source = new_dest
         else:
-            destination = args.destination
-        main_menu(destination)
+            source = args.source
+        if not os.path.isdir(source):
+            print(f"no directory {source} exists, exiting program.")
+            break
+        main_menu(source)
         choice = input('\nEnter your option: ').strip().upper()
         if choice == '1':
-            copy_to_stage(destination)
+            copy_to_stage(source)
         elif choice == '2':
-            rename_files(destination)
+            rename_files(source)
         elif choice == '3':
-            new_dest = rename_stage(destination)
+            new_dest = rename_stage(source)
             if new_dest is not None:
                 stage_dir = new_dest
             else:
                 stage_dir = ''
         elif choice == '4':
-            move_to_top(destination)
+            move_to_top(source)
         elif choice == '5':
-            csv_file = get_mediainfo(destination)
+            csv_file = get_mediainfo(source)
             arrange_csv(csv_file,
                 ['type', 'fileset_label', 'pcdm_use',
                  'preservation_master_file', 'extent',
@@ -559,12 +579,6 @@ def main(args_):
             break
         else:
             print(' - Incorrect input. Please enter 1, 2, 3, 4, 5, or Q')
-
-# #   prepare destination directory
-# #       connect to LIB-AV server (requires static IP, so can only be done from front audio workstation)
-# #       check if a directory already exists for the collection (or series/record group for EUA, Oxford, and Pitts)
-# #       if it doesn't, under the directory for the appropriate library, create a directory for the collection:
-# #           ex: MSS1256 (for EUA, Oxford, and Pitts use SER or RG instead of MSS as needed)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
